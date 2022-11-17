3da6f4147d0ce58412cb3417c4a93ee96f71b5ab
C.J <58392397+cleverjay308@users.noreply.github.com>
2022-11-17 13:23:54 +0100

    Update script.js

--- Color flipper/script.js
+++ Color flipper/script.js
@@ -2,7 +2,7 @@ const colors = ["green", "blue", "#fff", "rgb(20, 200, 210)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 btn.addEventListener("click", function change () {
-    //get random number between 0 -4(array index notation)
+    //get random number between 0 -4(array index notation).
     const randomNumber = Math.floor(Math.random() * 5);
     color.textContent = colors[randomNumber];
     document.body.style.backgroundColor = colors[randomNumber];
