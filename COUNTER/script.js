9e09c2e91c4159f838e7533c3ddadb3a75844c31
C.J <58392397+cleverjay308@users.noreply.github.com>
2022-12-06 11:33:52 +0100

    My second Javascript project :)...
    This project entails buttons that increment, decrease or set a counter to 0. The color of the counter is coded to change depending on whether it is greater than, lesser than or equal to zero

---
+++ COUNTER/script.js
@@ -0,0 +1,39 @@
+//Set initial count::
+let count = 0;
+
+//Select your Objects
+const value = document.getElementById("value");
+const increase = document.querySelector(".increase");
+const decrease = document.querySelector(".decrease");
+const reset = document.querySelector(".reset");
+
+// Did the maths here
+increase.addEventListener("click", function() {
+    count++;
+    value.textContent = count;
+    changeColor();
+});
+decrease.addEventListener("click", function () {
+    count--;
+    value.textContent = count;
+    changeColor();
+});
+reset.addEventListener("click", function () {
+    count = 0;
+    value.textContent = count;
+    changeColor();
+});
+
+function changeColor() {
+    if (count > 0) {
+        value.style.color = "green";
+    }
+    else if (count < 0) {
+        value.style.color = "red";
+    }
+    else {
+        value.style.color = "#000";
+    }
+};
+
+ 
